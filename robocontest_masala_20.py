6a61bcda9f7c499ab60dea5f76aa367c94650fd6
Samirbek Jabborov <131766016+SamirbekJabbor@users.noreply.github.com>
2023-05-31 17:25:03 +0500

    Update robocontest_masala_20.py

--- robocontest_masala_20.py
+++ robocontest_masala_20.py
@@ -1,2 +1,37 @@
-n,k=map(int,input().split())
-print(k%n)
+# import the turtle module
+import turtle
+import time
+
+# set up the screen
+win = turtle.Screen()
+win.title("Snake Game")
+win.bgcolor("green")
+
+# create the snake
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+# define the main game loop
+while True:
+    win.update()
+
+    # move the snake
+    if head.direction == "up":
+        head.sety(head.ycor() + 20)
+    elif head.direction == "down":
+        head.sety(head.ycor() - 20)
+    elif head.direction == "left":
+        head.setx(head.xcor() - 20)
+    elif head.direction == "right":
+        head.setx(head.xcor() + 20)
+
+    # set up a delay
+    time.sleep(0.1)
+
+# start the game
+turtle.mainloop()
